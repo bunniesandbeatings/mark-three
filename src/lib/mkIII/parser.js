026344698b1e4fe62b28cdc4b933c945b2624193
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2020-10-03 16:11:32 -0400

    extract knob names

--- src/lib/mkIII/parser.js
+++ src/lib/mkIII/parser.js
@@ -4,6 +4,7 @@ import _ from "lodash"
 const START_OF_PARAMS = 0x14
 const PARAM_WIDTH = 0x2C
 const BUTTON_PARAM_OFFSET = 0x00
+const KNOB_PARAM_OFFSET = 0x0F
 
 const paramPos = index => {
     const start = START_OF_PARAMS + (index * PARAM_WIDTH)
@@ -17,6 +18,19 @@ const getParameter = (raw, index, offset = BUTTON_PARAM_OFFSET) =>
 const getTemplateName = raw =>
     toText(raw.slice(0x04, 0x14))
 
+const FIELD_NAME_START = 0x01
+const FIELD_NAME_END = 0x0A
+
+function parseButton(raw, button) {
+    const param = getParameter(raw, button.id, BUTTON_PARAM_OFFSET)
+    button.name = toText(param.slice(FIELD_NAME_START, FIELD_NAME_END))
+}
+
+function parseKnob(raw, knob) {
+    const param = getParameter(raw, knob.id, BUTTON_PARAM_OFFSET)
+    knob.name = toText(param.slice(FIELD_NAME_START, FIELD_NAME_END))
+}
+
 export const parseRawTemplate = ({state, id, raw}) => {
     const template = state.templates[id]
 
@@ -24,9 +38,12 @@ export const parseRawTemplate = ({state, id, raw}) => {
     state.templates[id] = template
 
     _.forEach(state.buttons, (button) => {
-        const param = getParameter(raw, button.id)
-        console.log(param)
-        button.name = toText(param.slice(0x01, 0x0A))
+        parseButton(raw, button)
+    })
+
+    _.forEach(state.knobs, (knob) => {
+        const param = getParameter(raw, knob.id, KNOB_PARAM_OFFSET)
+        knob.name = toText(param.slice(FIELD_NAME_START, FIELD_NAME_END))
     })
 
     return state
