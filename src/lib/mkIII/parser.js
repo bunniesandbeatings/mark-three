1587d862a90529965579e83da06740e6ed2c8fe6
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2020-10-03 12:32:30 -0400

    Get button names from raw data

--- src/lib/mkIII/parser.js
+++ src/lib/mkIII/parser.js
@@ -1,9 +1,9 @@
 import {toText} from "../../util/byte"
 import _ from "lodash"
-import {emptyTemplate} from './model'
 
 const START_OF_PARAMS = 0x14
 const PARAM_WIDTH = 0x2C
+const BUTTON_PARAM_OFFSET = 0x00
 
 const paramPos = index => {
     const start = START_OF_PARAMS + (index * PARAM_WIDTH)
@@ -11,33 +11,23 @@ const paramPos = index => {
 }
 
 // Gets a full parameter setting from the raw line
-const getParameter = (raw, index) =>
-    raw.slice(...paramPos(index))
+const getParameter = (raw, index, offset = BUTTON_PARAM_OFFSET) =>
+    raw.slice(...paramPos(index + offset))
 
 const getTemplateName = raw =>
     toText(raw.slice(0x04, 0x14))
 
-const getButton = (raw, index) => {
-    const param = getParameter(raw, index)
-    return {
-        name: toText(param.slice(0x01, 0x0A))
-    }
-}
-
-const getButtons = raw =>
-    _.range(0, 16)
-    .map((index) =>
-        getButton(raw, index))
-
 export const parseRawTemplate = ({state, id, raw}) => {
     const template = state.templates[id]
-console.log(template.name)
-    template.name = getTemplateName(raw)
 
+    template.name = getTemplateName(raw)
     state.templates[id] = template
+
+    _.forEach(state.buttons, (button) => {
+        const param = getParameter(raw, button.id)
+        console.log(param)
+        button.name = toText(param.slice(0x01, 0x0A))
+    })
+
     return state
-    // {
-    //     name:
-    //     // buttons: getButtons(raw),
-    // }
 }
